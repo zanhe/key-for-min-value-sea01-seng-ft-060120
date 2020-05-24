019af88cac8ef3bb2ce55794394d3f6fbd65b7b8
Zana H <zhertica@gmail.com>
2020-05-24 21:09:04 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,7 +5,7 @@ def key_for_min_value(name_hash)
   smallest_value = nil
   smallest_key = nil
   name_hash.each do |k, value|
-    if value < smallest_value || smallest_value = nil
+    if smallest_value = nil || value < smallest_value
       smallest_value = value
       smallest_key = k
     end
