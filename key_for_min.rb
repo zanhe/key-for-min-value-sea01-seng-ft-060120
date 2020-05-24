6bad7e1cecd6f47ccaa2f33d81c7aeee3668b943
Zana H <zhertica@gmail.com>
2020-05-24 20:57:30 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,10 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  smallest_value = nil
+  name_hash.each do |key, value|
+    if value < smallest_value
+      
+    end
+  end
 end
